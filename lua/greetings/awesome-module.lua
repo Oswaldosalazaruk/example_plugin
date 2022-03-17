d2e4a27f286b070512e55359ae3df7251501f49f
oswaldo Salazar <oswaldosalazar.uk@gmail.com>
2022-03-17 11:16:19 +0000

    my first commit

---
+++ lua/greetings/awesome-module.lua
@@ -0,0 +1,8 @@
+--!/usr/bin/lua
+local function greet()
+  print('mi primer plugin')
+end
+
+return greet
+
+
---
+++ lua/greetings/init.lua
@@ -0,0 +1,6 @@
+--!/usr/bin/lua
+
+local greet = require('greetings.awesome-module')
+return {
+  greet = greet
+}
